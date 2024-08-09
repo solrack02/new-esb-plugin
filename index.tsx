73ad74c41a4cfa879b1a4ab9b3a0194c2c248593
Carlos A. <carlosvsk@gmail.com>
2024-08-09 15:33:45 -0300

    Commit generated by Flaxboll.

--- index.tsx
+++ index.tsx
@@ -9,20 +9,9 @@ import * as RN from 'react-native';
 import { create } from 'zustand';
 
 // ---------- import Local Tools
-import {mapElements} from './tools/base/project/mapElements';
-import * as functions from './tools/base/functions';
-import * as Elements from './tools/base/Elements';
-import { Project } from './tools/base/project/';
-import * as jsvals from './tools/base/jsvals';
-import { props } from './tools/base/props';
-import * as customs from './tools/customs';
-import * as stls from './tools/base/stls';
-import { tools } from './tools';
 
 // ---------- set Caps Inputs
 const currRoute = 'ab0login'
-
-let args = [];
 
 const screens = [
   
@@ -31,26 +20,16 @@ const screens = [
 const initCt = () => ({
   true: "true"
 });
-const initObj = initCt();
-// console.log(initObj);
 
 const arrInitFuncs = [
   ()=>{}
 ];
 
-export const useRoutes = create(() => ({ currRoute }));
-
 // ---------- set Main Component
 export default function Router() {
 
   return (
-    <Project
-      configData={{
-        screens,
-        initCt,
-        arrInitFuncs,
-      }}
-    />
+    <RN.Text>Ola OLD!</RN.Text>
   );
 };
 
