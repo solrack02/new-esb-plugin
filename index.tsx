ab5af0de26c06e6c938bea63f4d5e0c763cbf2d1
Carlos A. <carlosvsk@gmail.com>
2024-08-12 14:45:06 -0300

    Commit generated by Flaxboll.

--- index.tsx
+++ index.tsx
@@ -1,15 +1,10 @@
 
             // file: index.tsx
-            "// ---------- import React Packs
-import React from 'react';
+            "import React from 'react';
 import * as RN from 'react-native';
 
-// ---------- import Variables Pack
 import { create } from 'zustand';
 
-// ---------- import Local Tools
-
-// ---------- set Caps Inputs
 const currRoute = '!#!initial screen!#!'
 
 const screens = [
@@ -24,7 +19,6 @@ const arrInitFuncs = [
   !#!Start Functions!#!
 ];
 
-// ---------- set Main Component
 export default function Router() {
 
   return (
