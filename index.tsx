732e47e720a73a828681d10269e98edfacef4322
Carlos A. <carlosvsk@gmail.com>
2024-08-12 17:05:52 -0300

    Commit generated by Flaxboll.

--- index.tsx
+++ index.tsx
@@ -1,13 +1,31 @@
 
             // file: index.tsx
-            ""import React from 'react';
+            ""// ---------- import React Packs
+import React from 'react';
 import * as RN from 'react-native';
-import { testModule } from './src/index.ts';
 
+// ---------- import Variables Pack
+import { create } from 'zustand';
+
+// ---------- import Local Tools
+
+// ---------- set Caps Inputs
+const currRoute = '!#!initial screen!#!'
+
+const screens = []
+
+const initCt = () => ({});
+
+const arrInitFuncs = [];
+
+// ---------- set Main Component
 export default function Router() {
+
   testModule();
 
-  return (<RN.Text>Ola OLD!</RN.Text>);
+  return (
+    <RN.Text>Ola OLD!</RN.Text>
+  );
 };
 
 RN.AppRegistry.registerComponent("Router", () => Router);
