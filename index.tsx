42b456ae60ba23345aa7a13eadce2e842be781f2
Carlos A. <carlosvsk@gmail.com>
2024-08-12 14:45:52 -0300

    Commit generated by Flaxboll.

--- index.tsx
+++ index.tsx
@@ -1,34 +1,4 @@
 
             // file: index.tsx
-            "import React from 'react';
-import * as RN from 'react-native';
-
-import { create } from 'zustand';
-
-const currRoute = '!#!initial screen!#!'
-
-const screens = [
-  !#!Lista de Telas!#!
-]
-
-const initCt = () => ({
-  !#!Lista de Variáveis!#!
-});
-
-const arrInitFuncs = [
-  !#!Start Functions!#!
-];
-
-export default function Router() {
-
-  return 
-    <RN.Text>Ola OLD!</RN.Text>
-  );
-};
-
-RN.AppRegistry.registerComponent("Router", () => Router);
-
-RN.AppRegistry.runApplication("Router", {
-rootTag: document.getElementById("root"),
-});("
+            ""
         
