dd837238bf08fe3936bb228202a1f7d9b068c2bf
Carlos A. <carlosvsk@gmail.com>
2024-08-12 17:03:48 -0300

    Commit generated by Flaxboll.

--- index.tsx
+++ index.tsx
@@ -1,18 +1,4 @@
 
             // file: index.tsx
-            ""import React from 'react';
-import * as RN from 'react-native';
-import { testModule } from './src/index.ts';
-
-export default function Router() {
-  testModule();
-
-  return (<RN.Text>Ola OLD!</RN.Text>);
-};
-
-RN.AppRegistry.registerComponent("Router", () => Router);
-
-RN.AppRegistry.runApplication("Router", {
-rootTag: document.getElementById("root"),
-});""
+            """"
         
