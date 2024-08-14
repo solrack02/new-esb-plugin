5507858afc26754ca7f9cedaa0490325d4d9e714
Carlos A. <carlosvsk@gmail.com>
2024-08-14 14:27:45 -0300

    Commit generated by Flaxboll.

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -1,18 +1,4 @@
 
-
-import React from 'react';
-import { Text, View } from 'react-native';
-import { DynView } from './src/tools/DynView.tsx';
- 
-const elements = {
-    DynView
-};
- export const tools = () => {
-  console.log('Welcome to the tools!');
-
-  const returnObj = {
-    elements,
-  };
-
-  return returnObj;
-};
+export const tools = () => {
+    console.log("Welcome to the tools!")
+}
