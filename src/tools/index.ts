34706708004b5e617dd4e8b7e6654b48787f8bef
Carlos A. <carlosvsk@gmail.com>
2024-08-14 12:23:07 -0300

    Commit generated by Flaxboll.

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -3,18 +3,7 @@
 import React from 'react';
 import { Text, View } from 'react-native';
  
-
-export const DynView = () => {
-  
-  return (
-    <>
-      <View style={{ height: 10 }} />
-      <Text style={{ color: 'white' }}>{'DynView'}</Text>
-    </>
-  );
-};
-
- const elements = {
+const elements = {
     DynView
 };
  export const tools = () => {
