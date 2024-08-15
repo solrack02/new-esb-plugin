d0628bced70d058baa8d456ea280a3e59fd708b2
Carlos A. <carlosvsk@gmail.com>
2024-08-15 14:59:17 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,2 @@
 
-// ---------- import React Packs
-import React from 'react';
-import { Text, View } from 'react-native';
-import { tools } from './tools';
-
-export const Router = () => {
-  tools();
-
-  return (
-    <View style={{ width: '100%', height: '100%', backgroundColor: 'indigo' }}>
-      <Text style={{ color: 'white' }}>{'Hi from:'}</Text>
-      <View style={{ height: 10 }} />
-      <Text style={{ color: 'white' }}>{'The Router 1!'}</Text>
-    </View>
-  );
-};
+console.log("AQUI X");
