d7bae11f39348d4b0e346fcabba8a9b9f35beba3
Carlos A. <carlosvsk@gmail.com>
2024-08-15 11:42:39 -0300

    Commit generated by Flaxboll.

---
+++ src/tools/base/elements/index.ts
@@ -0,0 +1,50 @@
+
+// ---------- import Internals
+// import { TcapsCodeData } from '../../../types/Tcaps/TcapsCode';
+// import { ccc_functionsLibrary } from './ccc_functionsLibrary';
+// import { ccc_elementsLibrary } from './ccc_elementsLibrary';
+// import { ccc_stylesLibrary } from './ccc_stylesLibrary';
+// import { ccc_screen } from './ccc_screen';
+
+// import { ccc_googlePlaces } from './ccc_googlePlaces';
+// import { ccc_textInput } from './ccc_textInput';
+
+// import { ccc_pressable } from './ccc_pressable';
+// import { ccc_login } from './ccc_login';
+
+// import { ccc_textPicker } from './ccc_textPicker';
+// import { ccc_flatList } from './ccc_flatList';
+// import { ccc_flaxHome } from './ccc_flaxHome';
+// import { ccc_dynView } from './ccc_dynView';
+// import { ccc_svgIcon } from './ccc_svgIcon';
+// import { ccc_project } from './ccc_project';
+
+// import { ccc_customListener } from './ccc_customListener';
+
+// import { ccc_scrollBar } from './ccc_scrollBar';
+// import { ccc_loader } from './ccc_loader';
+// import { ccc_image } from './ccc_image';
+// import { ccc_text } from './ccc_text';
+
+// export const elements: TcapsCodeData = {
+export const elements: any = {
+  //   ...ccc_googlePlaces,
+  //   ...ccc_loader,
+  //   ...ccc_pressable,
+  //   ...ccc_login,
+  //   ...ccc_flatList,
+  //   ...ccc_flaxHome,
+  //   ...ccc_dynView,
+  //   ...ccc_image,
+  //   ...ccc_elementsLibrary,
+  //   ...ccc_functionsLibrary,
+  //   ...ccc_customListener,
+  //   ...ccc_stylesLibrary,
+  //   ...ccc_screen,
+  //   ...ccc_scrollBar,
+  //   ...ccc_textInput,
+  //   ...ccc_textPicker,
+  //   ...ccc_svgIcon,
+  //   ...ccc_project,
+  //   ...ccc_text,
+};
